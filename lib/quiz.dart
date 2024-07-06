45a29c9b0add558510488f2937e2ffdac2107160
unknown <aziz706797@gmail.com>
2024-07-06 19:32:43 +0500

    finish quiz_app

---
+++ lib/quiz.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:lesson_3/question_screen.dart';
+import 'package:lesson_3/results_screen.dart';
+import 'package:lesson_3/start_screen.dart';
+
+import 'data/question.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  final List<String> _selectedAnswers = [];
+  var _activeScreen = 'start-screen';
+
+  void _switchScreen() {
+    setState(() {
+      _activeScreen = 'questions-screen';
+    });
+  }
+
+  void _chooseAnswer(String answer) {
+    _selectedAnswers.add(answer);
+
+    if (_selectedAnswers.length == questions.length) {
+      setState(() {
+        _activeScreen = 'results-screen';
+      });
+    }
+  }
+
+  void restartQuiz() {
+    setState(() {
+      _selectedAnswers.clear();
+      _activeScreen = 'questions-screen';
+    });
+  }
+
+  void finishQuiz() {
+    setState(() {
+      _selectedAnswers.clear();
+      _activeScreen = 'start-screen';
+    });
+  }
+
+  @override
+  Widget build(context) {
+    Widget screenWidget = StartScreen(_switchScreen);
+
+    if (_activeScreen == 'questions-screen') {
+      screenWidget = QuestionsScreen(
+        onSelectAnswer: _chooseAnswer,
+      );
+    }
+
+    if (_activeScreen == 'results-screen') {
+      screenWidget = ResultsScreen(
+        chosenAnswers: _selectedAnswers,
+        onRestart: restartQuiz,
+        onFinish: finishQuiz
+      );
+    }
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 78, 13, 151),
+                  Color.fromARGB(255, 107, 15, 168),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            child: screenWidget,
+          ),
+        ),
+      ),
+    );
+  }
+}
